bcbb4b05dc45c67e712d1daa2ce60f9f5e811662
mihajlo-cirikovacki <cirikovackimihajlo@yahoo.com>
2021-10-21 20:41:05 +0200

    Added JS/modal

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,36 @@
 'use strict';
 
-console.log('HELLO');
+const btnHeader = document.querySelector('.btn--header');
+const modalSupp = document.querySelector('.section-support');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.section-support__close');
+
+// FUNCTIONS:
+const closeModal = function() {
+  modalSupp.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+}
+
+
+
+// EVENT LISTENERS:
+btnHeader.addEventListener('click', closeModal);
+
+
+closeModalBtn.addEventListener('click', (e) => {
+  const icon = e.target.closest('.section-support__close');
+  if(!icon) return;
+  closeModal();
+});
+
+document.addEventListener('keydown', (e) => {
+  if(e.key === 'Escape') closeModal();
+})
+
+
+
+
+
+
+
 //# sourceMappingURL=script.js.map
